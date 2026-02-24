8909520c33c0f8d02df339bc1a610dc7c346163d
thekr1d <epifanov.vv.spb@gmail.com>
2026-02-24 12:05:04 +0300

    add src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+export const hello = () => {
+
+    console.log('Welcome to the Brain Games!');
+
+    const name = readlineSync.question('May I have your name?: ');
+    const helloString = `Hello, ${name}!`
+
+    console.log(helloString)
+};
